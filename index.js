0acf26c183c8a258971d7f3ebb5d52b90e9fb2dd
congvinh <congvinh@vinh>
2021-06-12 16:12:46 +0700

    changed port into index file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const userRoute = require('./route/user.route');
 const authRoute = require('./route/auth.route');
 const cookieParser = require('cookie-parser')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const authValidations = require('./validations/auth.validation');
 
